92fdc5736a52520a15f2b1b7d727bdcdf7812809
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-10-31 16:35:15 +0530

    Task List

--- Workspace/02-admin-dashboard/script.js
+++ Workspace/02-admin-dashboard/script.js
@@ -1,18 +1,4 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function () {
+  // select element having data-toggle attribute value as 'tooltip'
+  $('[data-toggle="tooltip"]').tooltip();
+});
