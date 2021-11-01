edb417dd256bc70064366fbab39578bd3110b613
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-11-01 11:04:50 +0530

    Navbar

--- Workspace/03-photox/script.js
+++ Workspace/03-photox/script.js
@@ -1,32 +1,19 @@
+$(document).ready(function () {
+  // togglng hamburger menu
+  $('.nav-button').click(function () {
+    $('.nav-button').toggleClass('change'); // add/remove 'change' class on click of nav-button
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // window scroll event
+  $(window).scroll(function () {
+    let position = $(this).scrollTop(); // calculates position in px when we scroll down
+    console.log(position);
+    if (position >= 200) {
+      // add custom-navbar class when scroll position is >= 200
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      // remove custom-navbar class when scroll position is < 200
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+  });
+});
