f06c49ffa68526d37b6c68a0e415e9590d5d463f
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-11-05 17:13:37 +0530

    Portfolio project - banner

---
+++ Workspace/04-portfolio/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // togglng hamburger menu
+  $('.nav-button').click(function () {
+    $('.nav-button').toggleClass('change'); // add/remove 'change' class on click of nav-button
+  });
+});
