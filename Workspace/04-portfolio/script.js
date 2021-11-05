b46bd5eeea2bd13016142514dd6b3487057b8868
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-11-05 17:52:35 +0530

    Banner - setting active menu item on click

--- Workspace/04-portfolio/script.js
+++ Workspace/04-portfolio/script.js
@@ -3,4 +3,18 @@ $(document).ready(function () {
   $('.nav-button').click(function () {
     $('.nav-button').toggleClass('change'); // add/remove 'change' class on click of nav-button
   });
+
+  // set currently clicked menu item as active
+  $('.navbar-nav a').on('click', function () {
+    console.log('clicked on nav');
+    $('.navbar-nav').find('li a.nav-active').removeClass('nav-active');
+    $(this).addClass('nav-active');
+  });
+
+  // set home menu item as active when clicked on logo
+  $('#logo').on('click', function () {
+    console.log('clicked on logo');
+    $('.navbar-nav').find('li a.nav-active').removeClass('nav-active');
+    $('a[href$="#home"]').addClass('nav-active');
+  });
 });
