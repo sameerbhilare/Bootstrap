8d1bc29fd85fbd7786271c09c1cb93c050a76857
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-11-10 12:18:21 +0530

    About - animation for title, image and description

--- Workspace/04-portfolio/script.js
+++ Workspace/04-portfolio/script.js
@@ -21,15 +21,51 @@ $(document).ready(function () {
   // window scroll event - to animate banner
   $(window).scroll(function () {
     let position = $(this).scrollTop();
-    // console.log(position);
+    //console.log(position);
     if (position <= 350) {
+      // banner
       $('.header').css('opacity', '1'); // when user scrolls up
       $('.header').addClass('fromTopHeader');
       $('.home-social *').addClass('socialMedia');
     } else {
+      // banner
       $('.header').css('opacity', '0'); // when user scrolls up from bottom, we should not show the header to have better UX
       $('.header').removeClass('fromTopHeader');
       $('.home-social *').removeClass('socialMedia');
+    }
+  });
+
+  // window scroll event - to animate About
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    // console.log(position);
+    if (position > 350) {
+      // about
+      $('.about-title').css('opacity', '1'); // when user scrolls up
+      $('.about-title').addClass('sectionTitleAnimation');
+    } else {
+      // about
+      $('.about-title').css('opacity', '0'); // when user scrolls up from bottom, we should not show the header to have better UX
+      $('.about-title').removeClass('sectionTitleAnimation');
+    }
+  });
+
+  // window scroll event - to animate camera and text in the mission section
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    // console.log(position);
+    if (position > 350) {
+      $('.about-img').css('opacity', '1'); // when user scrolls up
+      $('.about-img').addClass('fromLeft');
+
+      $('.about-description').css('opacity', '1'); // when user scrolls up
+      $('.about-description').addClass('fromRight');
+    } else {
+      $('.about-img').css('opacity', '0'); // when user scrolls up from bottom, we should not show the header to have better UX
+      $('.about-img').removeClass('fromLeft');
+
+      $('.about-description').css('opacity', '0'); // when user scrolls up from bottom, we should not show the header to have better UX
+      $('.about-description').removeClass('fromRight');
     }
   });
 });
